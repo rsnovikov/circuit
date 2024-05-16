2f015dc98804ef304516d70efa2783137012a6da
Novikov <novikov@ppdp.ru>
2024-05-16 21:44:12 +0300

    добавил сабмодуль MNA

--- src/features/circuitTools/StartModelingBreadboardTool.tsx
+++ src/features/circuitTools/StartModelingBreadboardTool.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
-import { useAppDispatch } from '@/shared/model';
+import { useAppDispatch, useAppSelector } from '@/shared/model';
 import { notify } from '@/shared/notification';
 import { BreadboardToolsBtn } from './BreadboardToolsBtn';
-
+import { analyzeCircuit } from '../../../MNA/test.js';
 export const StartModelingBreadboardTool: FC = () => {
   const dispatch = useAppDispatch();
+  const nodes = useAppSelector(state => state.node.nodes);
+  const elements = useAppSelector(state => state.cirElement.elements);
   const handleClick = () => {
+    console.log(analyzeCircuit({nodes, elements}))
     dispatch(notify({ message: 'Пока не реализовано :(', type: 'info' }));
   };
 
