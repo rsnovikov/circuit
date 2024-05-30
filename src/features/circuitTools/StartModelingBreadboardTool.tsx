c0986ea441046f4fb861bd08ff032735a3c3ef63
Valeriy Vinogradov <valera.krosava@gmail.com>
2024-05-30 20:04:13 +0300

    added toFixed to PhysicalData (#14)

--- src/features/circuitTools/StartModelingBreadboardTool.tsx
+++ src/features/circuitTools/StartModelingBreadboardTool.tsx
@@ -2,14 +2,28 @@ import { FC } from 'react';
 import { useAppDispatch, useAppSelector } from '@/shared/model';
 import { notify } from '@/shared/notification';
 import { BreadboardToolsBtn } from './BreadboardToolsBtn';
-import { analyzeCircuit } from 'MNA';
+import { analyzeCircuit } from 'MNA/analyzeCircuit';
+import { setCirElements } from '@/entities/cirElement/model/slice';
+import { CircuitData } from '@/entities/circuit/api/types';
+import { ICirElement } from '@/entities/cirElement/model/types';
+
 export const StartModelingBreadboardTool: FC = () => {
   const dispatch = useAppDispatch();
   const nodes = useAppSelector(state => state.node.nodes);
   const elements = useAppSelector(state => state.cirElement.elements);
   const handleClick = () => {
-    console.log(analyzeCircuit({nodes, elements}))
-    dispatch(notify({ message: 'Пока не реализовано :(', type: 'info' }));
+    const currentData: CircuitData = JSON.parse(JSON.stringify({ nodes, elements }))
+    try {
+      const elementsToChange: ICirElement[] = analyzeCircuit(currentData)
+      currentData.elements = currentData.elements.map(e => {
+        const index: number = elementsToChange.findIndex(element => element.id === e.id)
+        return index >= 0 ? elementsToChange[index] : e;
+      })
+      console.log(currentData)
+      dispatch(setCirElements(currentData.elements));
+    } catch (error) {
+      dispatch(notify({ message: 'НАСРАЛ ГОВНА', type: 'error' }));
+    }
   };
 
   return (
