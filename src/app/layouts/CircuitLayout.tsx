4d97b201b1c6cd29209fc7677b7e50ff72e7ba64
Novikov <novikov@ppdp.ru>
2024-06-04 02:28:43 +0300

    доступ из анонимной сессии

--- src/app/layouts/CircuitLayout.tsx
+++ src/app/layouts/CircuitLayout.tsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom';
 import { ChangeCircuitNameForm } from '@/features/breadboard/changeCircuitName';
 import { SaveBreadboard } from '@/features/breadboard/saveBreadboard';
 import { Header } from '@/widgets/header';
+import { useAppSelector } from "@/shared/model";
 
 export const CircuitLayout: FC = () => {
+  const isAuthorized = useAppSelector(state => state.auth.isAuthorized);
   return (
     <div className="w-full h-full overflow-hidden">
-      <Header leftSlot={<ChangeCircuitNameForm />} rightSlot={<SaveBreadboard />} />
+      <Header leftSlot={<ChangeCircuitNameForm />} rightSlot={isAuthorized && <SaveBreadboard />} />
       <Outlet />
     </div>
   );
--- src/widgets/header/ui/HeaderLogo.tsx
+++ src/widgets/header/ui/HeaderLogo.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from '@/shared/ui/Icon/Icon';
+import { useAppSelector } from "@/shared/model";
 
 export const HeaderLogo: FC = () => {
+  const isAuthorized = useAppSelector((state) => state.auth.isAuthorized);
+
   return (
-    <Link to="/dashboard">
+    <Link to={isAuthorized ? "/dashboard" : 'circuit'}>
       <Icon
         type="CircuitBoard"
         width={40}
